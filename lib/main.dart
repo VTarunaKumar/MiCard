7f1012684f53a1a5f7c5825fa22bd7c29c7e7a60
VTarunaKumar <vtarunakumarrao>
2022-09-01 12:14:10 +0530

    1/09/2022

--- lib/main.dart
+++ lib/main.dart
@@ -10,39 +10,31 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.black12,
+        backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Row(
-            // verticalDirection: VerticalDirection.up,
-            // mainAxisSize: MainAxisSize.min,
-            // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            // crossAxisAlignment: CrossAxisAlignment.end,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
+          child: Column(
             children: <Widget>[
-              Container(
-                // height: 100,
-                width: 20,
-                color: Colors.white,
-                child: Text("Container 1"),
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage("Images/me.JPG"),
               ),
-              SizedBox(
-                height: 100,
+              Text(
+                "V Taruna Kumar Rao",
+                style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    fontSize: 40.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
               ),
-              Container(
-                // width: double.infinity,
-                // height: 100,
-                color: Colors.green,
-                child: Text("Container 2"),
-              ),
-              Container(
-                // width: double.infinity,
-                // height: 100,
-                color: Colors.red,
-                child: Text("Container 3"),
-              ),
-              // Container(
-              //   width: double.infinity,
-              // )
+              Text(
+                "Flutter Devloper",
+                style: TextStyle(
+                    fontFamily: "SourceSansPro",
+                    fontSize: 30.0,
+                    color: Colors.teal.shade100,
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold),
+              )
             ],
           ),
         ),
