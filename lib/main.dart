6de09694669736d90857945fc62fa46cd6d24dda
VTarunaKumar <vtarunakumarrao>
2022-09-01 14:16:25 +0530

    1/09/2022

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.blue[300],
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               CircleAvatar(
                 radius: 50,
@@ -34,7 +35,45 @@ class MyApp extends StatelessWidget {
                     color: Colors.teal.shade100,
                     letterSpacing: 2.5,
                     fontWeight: FontWeight.bold),
-              )
+              ),
+              SizedBox(
+                height: 20,
+                width: 200,
+                child: Divider(
+                  color: Colors.lightBlueAccent,
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+                child: ListTile(
+                    leading: Icon(Icons.phone, color: Colors.teal),
+                    title: Text(
+                      "+91 7077264489",
+                      style: TextStyle(
+                          color: Colors.black54,
+                          fontFamily: "SourceSansPro",
+                          fontSize: 20),
+                    )),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    "vtarunakumarrao@gmail.com",
+                    style: TextStyle(
+                      fontFamily: "SourceSansPro",
+                      fontSize: 20,
+                      color: Colors.black54,
+                    ),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
